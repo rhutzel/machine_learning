b7628872d7be9d0f2aecc23df36a4f964b668a84
Robert Hutzel <Robert.Hutzel@vistronix.com>
2017-04-21 14:19:13 -0400

    Tests the prediction and recall score against a candidate threshold.

--- ml_classification/driver.py
+++ ml_classification/driver.py
@@ -1,7 +1,7 @@
 import mnist_utils
 import test_set_utils
 from sklearn.linear_model import SGDClassifier
-from sklearn.metrics import confusion_matrix, precision_recall_curve
+from sklearn.metrics import confusion_matrix, precision_recall_curve, precision_score, recall_score
 from sklearn.model_selection import cross_val_predict
 
 
@@ -21,4 +21,8 @@ print("Confusion matrix: ", confusion_matrix(y_train_5, y_train_predictions))
 y_scores = cross_val_predict(stoch_grad_desc_cls, X_train, y_train_5, cv=3, method="decision_function")
 precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
 test_set_utils.plot_precision_recall(precisions, recalls, thresholds)
+
+# Decide that a threshold of around 70,000 will get really good precision.
+print("\n70,000 Precision: ", precision_score(y_train_5, (y_scores > 70000)),
+      "\n70,000 Recall: ", recall_score(y_train_5, (y_scores > 70000)))
 
